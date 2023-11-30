cfaf6afa54922ec3b090b05b4cd195d4c4bfd472
soobin70 <142298498+soobin70@users.noreply.github.com>
2023-11-30 09:47:42 +0900

    review작성 제작중

--- server/src/main/java/com/yummsters/cafehub/domain/review/controller/ReviewController.java
+++ server/src/main/java/com/yummsters/cafehub/domain/review/controller/ReviewController.java
@@ -1,13 +1,37 @@
 package com.yummsters.cafehub.domain.review.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.yummsters.cafehub.domain.review.dto.ReviewDto;
 import com.yummsters.cafehub.domain.review.service.ReviewService;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ReviewController {
+	@Autowired
+	private ReviewService reviewService;
+	@Autowired
 	
+	@PostMapping("/reviewwrite")
+	public ResponseEntity<Integer> reviewWrite(@ModelAttribute ReviewDto review,
+	                                          @RequestParam("file") List<MultipartFile> files) {
+	    try {
+	        Integer reviewNo = reviewService.reviewWrite(review, files);
+	        return new ResponseEntity<>(reviewNo, HttpStatus.OK);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	    }
+	}
 	
 
 }
--- server/src/main/java/com/yummsters/cafehub/domain/review/service/ReviewService.java
+++ server/src/main/java/com/yummsters/cafehub/domain/review/service/ReviewService.java
@@ -1,12 +1,16 @@
 package com.yummsters.cafehub.domain.review.service;
 
 import java.io.OutputStream;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import com.yummsters.cafehub.domain.review.dto.ReviewDto;
 
 public interface ReviewService {
 	ReviewDto reviewDetail(Integer reviewNo) throws Exception;
+	Integer reviewWrite(ReviewDto review,List<MultipartFile> file) throws Exception;
 	//썸네일 이미지
 	void thumbImg(Integer reviewNo, OutputStream out) throws Exception;
-	//리뷰 삭제
+
 }
