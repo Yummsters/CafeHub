a1d817ed4e9e04f844bdc52d74b7f81b8b8e43e3
hyeri-seo <hye_dam_2@naver.com>
2023-12-01 14:14:08 +0900

    reply 오류 수정

--- server/src/main/java/com/yummsters/cafehub/domain/reply/repository/ReplyRepository.java
+++ server/src/main/java/com/yummsters/cafehub/domain/reply/repository/ReplyRepository.java
@@ -1,9 +1,11 @@
 package com.yummsters.cafehub.domain.reply.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yummsters.cafehub.domain.reply.entity.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Integer> {
-
+	Optional<Reply> findByReplyByNo(Integer replyNo);
 }
--- server/src/main/java/com/yummsters/cafehub/domain/reply/service/ReplyService.java
+++ server/src/main/java/com/yummsters/cafehub/domain/reply/service/ReplyService.java
@@ -2,5 +2,5 @@ package com.yummsters.cafehub.domain.reply.service;
 
 public interface ReplyService {
 	void replyDelete(Integer replyNo) throws Exception;
-
+	
 }
