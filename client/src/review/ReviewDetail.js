e77fa7bd8e11fe39ad1da6b60e4718f8bdb2ebd0
Seonjin Heo <seonjin.dev@gmail.com>
2023-12-03 17:04:26 +0900

    feat : review 추천하기 기능

--- client/src/review/ReviewDetail.js
+++ client/src/review/ReviewDetail.js
@@ -7,17 +7,35 @@ const ReviewDetail = () => {
   const [review, setReview] = useState(null);
   const [showReply, setShowReply] = useState(false);
   const [like, setLike] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
   const [wish, setWish] = useState(false);
   const reviewNo = 1;
+  const memNo = 1;
   const showReplyClick = () => {
     setShowReply(!showReply);
   };
+  const toggleLike = () => {
+    axios.get(`http://localhost:8080/like/${memNo}/${reviewNo}`)
+      .then((res) => {
+        setLike(res.data.toggleLike);
+        setLikeCount(res.data.likeCount);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error("에러:" + error);
+      });
+  }
+  const toggleWish = () => {
+    setWish(!wish);
+  }
 
   useEffect(() => { // 디테일 가져오기
     axios
       .get(`http://localhost:8080/review/${reviewNo}`)
       .then((res) => {
-        setReview(res.data);
+        setReview(res.data.review);
+        setLike(res.data.isLike);
+        setLikeCount(res.data.review.likeCount);
         console.log(res.data);
       })
       .catch((error) => {
@@ -55,11 +73,11 @@ const ReviewDetail = () => {
                   <img src="/img/house.png" alt="house" />
                   {review.cafeName}
                 </p>
-                <p>{review.tagNames.map((tag) => <span>#{tag}&nbsp;</span>)}</p>
+                <p>{review.tagNames.map((tag, i) => <span key={i}>#{tag}&nbsp;</span>)}</p>
               </div>
               <div className="infoR">
                 <span>{review.nickname}</span>&nbsp;|&nbsp;
-                <span>추천 {review.likeCount}</span>
+                <span>추천 {likeCount}</span>
                 <p>{review.regDate}</p>
               </div>
             </div>
@@ -68,8 +86,8 @@ const ReviewDetail = () => {
             <div id="detailMap"></div>
 
             <div className="starNheart">
-              <img src={wish ? "/img/y_star.png" : "/img/n_star.png"} alt="star" /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <img src={like ? "/img/y_heart.png" : "/img/n_heart.png"} alt="heart" />
+              <img src={wish ? "/img/y_star.png" : "/img/n_star.png"} alt="star" onClick={toggleWish} /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <img src={like ? "/img/y_heart.png" : "/img/n_heart.png"} alt="heart" onClick={toggleLike} />
             </div>
             <div className="detailBtnBox">
               <div className="Gbtn">수정</div>
