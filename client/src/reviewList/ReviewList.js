20cdfd68bda910dc43a67e61afbadf855c9bf01e
hyeri-seo <hye_dam_2@naver.com>
2023-12-08 10:16:18 +0900

    ReviewList 수정

--- client/src/reviewList/ReviewList.js
+++ client/src/reviewList/ReviewList.js
@@ -7,198 +7,61 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const ReviewList = () => {
-    const reviewNo = 1; // 수정 필요
+
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/reviewList`)
+            .then((response) => {
+                setReviews(response.data);
+            })
+            .catch((error) => {
+                console.error('리뷰 가져오기 오류:', error);
+            });
+    }, []); // 빈 배열을 전달하여 컴포넌트가 처음 로드될 때만 useEffect가 실행되도록 설정
+
     return (
         <div className='reviewWrapper'>
-        <div className='reviewListBox'>
-            <div className='searchBar'>
-                <input className='searchBox'></input>
-                <img src='/img/searchIcon.png' alt=''/>
-            </div>
-                    <div className='reviewline'/>
-                    <a href='/reviewwrite'><button className='reviewBtn'>리뷰 등록</button></a>
-                    
-             <div className='reviewtable'>
-            <Table hover >
-                <tbody>
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad1.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                    <Link to={`/reviewDetail/${reviewNo}`}>
-                        <div className='listMiniTitle'>따뜻한 느낌의 책 읽기 좋은 카페</div>
-                    </Link>
-                        <div className='description1'>따뜻한 카페</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/cookies.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.15 11:01</div>
-                    </td>
-                    </tr>
-                    
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad2.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                        <div className='listMiniTitle'>편하게 이야기 나눌 수 있는 곳 발견</div>
-                        <div className='description1'>편한 이야기 카페</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/cupcake.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.15 21:14</div>
-                    </td>
-                    </tr>
+            <div className='reviewListBox'>
+                <div className='searchBar'>
+                    <input className='searchBox'></input>
+                    <img src='/img/searchIcon.png' alt='' />
+                </div>
+                <div className='reviewline' />
+                <a href='/reviewwrite'><button className='reviewBtn'>리뷰 등록</button></a>
 
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad3.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                        <div className='listMiniTitle'>베리가 가득해 더욱 맛있는</div>
-                        <div className='description1'>여수밤바다</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/icecream.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.16 12:30</div>
-                    </td>
-                    </tr>
-
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad4.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                        <div className='listMiniTitle'>추천합니다 감성카페</div>
-                        <div className='description1'>혜리 집</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/macaroon.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.17 10:03</div>
-                    </td>
-                    </tr>
-
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad5.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                        <div className='listMiniTitle'>추천합니다 안주가 맛있는 카페</div>
-                        <div className='description1'>선진 집</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/pancake.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.18 18:30</div>
-                    </td>
-                    </tr>
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad5.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                        <div className='listMiniTitle'>추천합니다 안주가 맛있는 카페</div>
-                        <div className='description1'>선진 집</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/pancake.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.18 18:30</div>
-                    </td>
-                    </tr>
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad5.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                        <div className='listMiniTitle'>추천합니다 안주가 맛있는 카페</div>
-                        <div className='description1'>선진 집</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/pancake.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.18 18:30</div>
-                    </td>
-                    </tr>
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad5.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                        <div className='listMiniTitle'>추천합니다 안주가 맛있는 카페</div>
-                        <div className='description1'>선진 집</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/pancake.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.18 18:30</div>
-                    </td>
-                    </tr>
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad5.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                        <div className='listMiniTitle'>추천합니다 안주가 맛있는 카페</div>
-                        <div className='description1'>선진 집</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/pancake.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.18 18:30</div>
-                    </td>
-                    </tr>
-                    <tr>
-                    <th scope="row">
-                        <img className='listImg' src='/img/Ad5.png' alt=''/>
-                    </th>
-                    <td colSpan={10}>
-                        <div className='listMiniTitle'>추천합니다 안주가 맛있는 카페</div>
-                        <div className='description1'>선진 집</div>
-                    </td>
-                    <td colSpan={2}>
-                        <div className='writeInfo'>
-                            <img src='/img/pancake.png' alt=''/>
-                            선진언니바보 | 추천 24
-                        </div>
-                        <div className='dateTime'>2023.11.18 18:30</div>
-                    </td>
-                    </tr>
-                <tr>
-              
-                </tr>
-                </tbody>
-            </Table>
+                <div className='reviewtable'>
+                    <Table hover >
+                        <tbody>
+                            {reviews.map((review) => (
+                                <tr key={review.reviewNo}>
+                                    <th scope='row'>
+                                        <img className='listImg' src={`/img/${review.thumbImg}.png`} alt='' />
+                                    </th>
+                                    <td colSpan={10}>
+                                        <Link to={`/reviewDetail/${review.reviewNo}`}>
+                                            <div className='listMiniTitle'>{review.title}</div>
+                                        </Link>
+                                        <div className='description1'>{review.tagName}</div>
+                                    </td>
+                                    <td colSpan={2}>
+                                        <div className='writeInfo'>
+                                            <img src='/img/cookies.png' alt='' />
+                                            {`${review.member.nickname} | 추천 ${review.likeCount}`}
+                                        </div>
+                                        <div className='dateTime'>{review.regDate}</div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
+                <div className='reviewList-pagination'>
+                    <div className='reviewList-prevPage'>&lt;</div>
+                    <div className='reviewList-page'>1 2 3 맵사용해~</div>
+                    <div className='reviewList-nextPage'>&gt;</div>
+                </div>
             </div>
-            <div className='reviewList-pagination'>
-                <div className='reviewList-prevPage'>&lt;</div>
-                <div className='reviewList-page'>1 2 3 맵사용해~</div>
-                <div className='reviewList-nextPage'>&gt;</div>
-            </div>
-        </div>
         </div>
     );
 };
