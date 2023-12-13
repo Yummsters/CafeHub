0fc5e6dc0a64233e847810c2267ad4f230ded6ce
Seonjin Heo <seonjin.dev@gmail.com>
2023-12-14 03:58:31 +0900

    feat : map 카페 선택 시 리뷰 목록

--- server/src/main/java/com/yummsters/cafehub/domain/cafe/service/CafeService.java
+++ server/src/main/java/com/yummsters/cafehub/domain/cafe/service/CafeService.java
@@ -15,6 +15,7 @@ public interface CafeService {
     CafeDto getCafeByCafeNo(Integer cafeNo) throws Exception; // 카페 하나의 정보 불러오기
     boolean isWishCafe(Integer memNo, Integer cafeNo) throws Exception; // 회원의 리뷰 찜 여부
     boolean toggleWishCafe(Integer memNo, Integer cafeNo) throws Exception; // 찜, 찜 취소
+    // 희진 part---------------------------------------------------------------------------
     Cafe searchCafe(Integer cafeNo) throws Exception;
     // 혜리 part---------------------------------------------------------------------------
     Page<CafeDto> getUnpaidCafes(Pageable pageable) throws Exception;
