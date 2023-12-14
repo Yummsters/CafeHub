89fde63a2f6ccfcecff76f2bd2e3ccc58217712b
hyeri-seo <hye_dam_2@naver.com>
2023-12-14 14:21:24 +0900

    userReview 필요 없는 코드 삭제

--- client/src/userReview/UserReview.js
+++ client/src/userReview/UserReview.js
@@ -1,57 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Pagination, PaginationLink } from "reactstrap";
+import { useSearchParams } from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import './userReviewStyle.css';
 import axios from 'axios';
 
 const UserReview = () => {
-    const accessToken = useSelector(state => state.persistedReducer.accessToken);
-
-    const [cafeNo, setCafeNo] = useState(1);
 
     const [reviewList, setReviewList] = useState([]);
     const [pageInfo, setPageInfo] = useState({page : 1, size : 5, totalElements : 1, totalPages : 1});
-    const [page, setPage] = useState(1);
-    const [curPage, setCurPage] = useState(page);
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    let firstNum = curPage - (curPage % 5) + 1;
-    let lastNum = curPage - (curPage % 5) + 5;
-    let total =  Math.min(4, (pageInfo.totalPages ===0 ? 1 : pageInfo.totalPages) - firstNum);
+    useEffect(() => {
+        setPageInfo({...pageInfo, currentPage: parseInt(searchParams.get('page'))});
+    }, [searchParams]);
 
-    useEffect(()=>{
-        getPage(page);
-    },[])
-    
-    // 페이지 조회
-    const getPage = (page) => {
-        setPage(page);
-        setCurPage(page);
-        axios.get(`http://localhost:8080/review/storeList/${cafeNo}?page=${page}&&size=5`,
-        {
-            headers : {
-                Authorization : accessToken
-            }
+    useEffect(() => {
+        axios.get(`http://localhost:8080/userReview`, {
+            params: {
+                page: pageInfo.currentPage - 1,
+                size: pageInfo.cafesPerPage,
+            },
         })
-        .then(res=>{
-            const list = res.data.data;
-            const resPageInfo = res.data.pageInfo;
-
-            setReviewList([...list]);
-            setPageInfo({
-                page: resPageInfo.page,
-                size: resPageInfo.size,
-                totalElements: resPageInfo.totalElements,
-                totalPages: resPageInfo.totalPages
-              });
-        })
-        .catch(err=>{
-            console.log(err);
-        })
-    }
+            .then((response) => {
+                console.log(response);
+                let totalPages = response.data.totalPages;
+                let startPage = Math.floor((pageInfo.currentPage - 1) / pageInfo.cafesPerPage) + 1;
+                let endPage = Math.min(startPage + pageInfo.cafesPerPage - 1, totalPages);
+                setPageInfo({ ...pageInfo, startPage: startPage, endPage: endPage, totalPages: totalPages });
+            })
+            .catch(error => {
+                console.error('미결제 카페 목록 가져오기 오류:', error);
+            });
+    }, [pageInfo.currentPage]);
 
     const reviewDetail = (reviewNo) =>{
         window.location.href = '/reviewDetail/'+reviewNo;
     }
+
+    const handlePageChange = (pageNumber) => {
+        setSearchParams((prev) => {
+            prev.delete('page');
+            prev.append('page', pageNumber);
+            return prev;
+        });
+    };
 
     return (
         <div className='userReview-container'>
@@ -74,52 +67,21 @@ const UserReview = () => {
                         </tbody>
                     </Table>
                 </div>
-                <Pagination className="userReview-Page">
-                    <PaginationLink 
-                    className='userReview-Button'
-                        onClick={() => {getPage(page-1); setCurPage(page-2);}} 
-                        disabled={page===1}>
-                        &lt;
-                    </PaginationLink>  
-                    <PaginationLink 
-                         className={`userReview-Button ${firstNum === page ? 'current-page' : ''}`}
-                        onClick={() => getPage(firstNum)}
-                        aria-current={page === firstNum ? "page" : null}>
-                        {firstNum}
-                    </PaginationLink>
-                    {Array(total).fill().map((_, i) =>{
-                        
-                    if(i <=2){
-                        let pageNum = firstNum+1+i;
-                        return (
-                            <PaginationLink
-                            className={`userReview-Button ${pageNum === page ? 'current-page' : ''}`}
-                            key={i+1} 
-                                onClick={() => {getPage(firstNum+1+i)}}
-                                aria-current={page === firstNum+1+i ? "page" : null}>
-                                {firstNum+1+i}
-                            </PaginationLink>
-                        )
-                    }else if(i>=3){
-                        let pageNum = lastNum;
-                         return (
-                            <PaginationLink
-                            className={`userReview-Button ${pageNum === page ? 'current-page' : ''}`}
-                            key ={i+1}
-                                onClick={() => getPage(lastNum)}
-                                aria-current={page === lastNum ? "page" : null}>
-                                {lastNum}
-                            </PaginationLink>
-                         )  
-                        }
-                    })}
-                    <PaginationLink 
-                      className='userReview-Button'
-                        onClick={() => {getPage(page+1); setCurPage(page);}} 
-                        disabled={page >=pageInfo.totalPages}>
-                        &gt;
-                    </PaginationLink>
-                </Pagination>
+                <div className='reviewList-pagination'>
+                    <ul className="pagination">
+                        <li className={`page-item ${pageInfo.currentPage === 1 ? 'disabled' : ''}`}>
+                            <button className="page-link" onClick={() => handlePageChange(pageInfo.currentPage - 1)}>&lt;</button>
+                        </li>
+                        {Array.from({ length: Math.ceil(pageInfo.endPage - pageInfo.startPage + 1) }, (_, index) => (
+                            <li key={index} className={`page-item ${pageInfo.currentPage === index + 1 ? 'active' : ''}`}>
+                                <button className="page-link" onClick={() => handlePageChange(index + pageInfo.startPage)}>{index + pageInfo.startPage}</button>
+                            </li>
+                        ))}
+                        <li className={`page-item ${pageInfo.currentPage === pageInfo.endPage ? 'disabled' : ''}`}>
+                            <button className="page-link" onClick={() => handlePageChange(pageInfo.currentPage + 1)}>&gt;</button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
