7452b07627662b16505b1f2d427f96311e6c8ff9
Seonjin Heo <seonjin.dev@gmail.com>
2023-12-17 00:33:08 +0900

    modify : 포인트 구매 수정

--- client/src/payment/Success.js
+++ client/src/payment/Success.js
@@ -1,53 +1,85 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 
 const Success = () => { // response 객체를 가지고 와서 추출하여 사용.
     const memNo = useSelector(state=>state.persistedReducer.member.memNo);
+    const accessToken = useSelector(state => state.persistedReducer.accessToken);
     const params = new URLSearchParams(window.location.search);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [isSuccess, setIsSuccess] = useState(false); // 결제 승인 여부
 
     const paymentData = {
         orderId: params.get('orderId'),
         paymentKey: params.get('paymentKey'),
         amount: params.get('amount'),
+        orderName: params.get('orderName'),
         memNo: memNo
     }
-    console.log(paymentData);
 
-useEffect(() => {
+    const onClick = () => {
+      navigate(-1);
+    }
+
+    console.log(paymentData.orderName);
+    useEffect(() => {
         axios
         .post('http://localhost:8080/payment/result', paymentData)
         .then((res) => {
             console.log(res);
-            window.location.href = '/userPoint';
+            setIsSuccess(true);
+            // dispatch({type:"payment", payload: { price: paymentData.amount, isSuccess: true }});
+            if (paymentData.orderName === '포인트구매') {
+              buyPoint();
+            }
         })
         .catch((error) => {
             console.log(error);
         })
     }, [])
 
+    const buyPoint = () => {
+      axios.post(`http://localhost:8080/point/buyPoint/${memNo}/${paymentData.amount/100}`,
+      {
+          headers : {
+              Authorization : accessToken
+          }
+      })
+      .then((res)=>{
+          console.log(res.data);
+      })
+      .catch((error) =>{
+          console.log(error);
+      })
+    }
+
     return (
     <div>
-      {/* {isSuccess === 'true' ? (
-        <div>
-          <h1>결제 성공</h1>
-          <p>결과데이터: {responseStr}</p>
-          <p>orderName: {orderName}</p>
-          <p>method: {method}</p>
-          {method === '카드' && <p>cardNumber: {cardNumber}</p>}
-          {method === '가상계좌' && <p>accountNumber: {accountNumber}</p>}
-          {method === '계좌이체' && <p>bank: {bank}</p>}
-          {method === '휴대폰' && <p>customerMobilePhone: {customerMobilePhone}</p>}
+      
+      {isSuccess ? (
+          <div className='paymentModal'>
+          <div className='paymentModal-content'>
+                <div className='payment-result-box'>
+                  <p className='payment-result'>{paymentData.amount}원 결제 완료</p>
+                  <button className='purchaseBtn' onClick={onClick}>확인</button>
+                </div>
+          </div>
         </div>
       ) : (
-        <div>
-          <h1>결제 실패</h1>
-          <p>에러메시지: {message}</p>
-          <p>에러코드: {code}</p>
+        <div className='paymentModal'>
+        <div className='paymentModal-content'>
+            <div className='payment-result-box'>
+              <p className='payment-result'>결제 실패</p>
+              <button className='purchaseBtn' onClick={onClick}>확인</button>
+            </div>
+          </div>
         </div>
-      )} */}
+      )}
     </div>
+
     );
 };
 
--- client/src/userMyPage/UserPoint.js
+++ client/src/userMyPage/UserPoint.js
@@ -1,13 +1,47 @@
-import React, { useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import './UserPointStyle.css';
 import UserSideTab from '../components/UserSideTab';
-import { useHref } from 'react-router';
-import { Link } from 'react-router-dom';
 import { CheckoutPage } from '../payment/CheckoutPage';
+import { PaymentContext } from '../payment/PaymentContext';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import Swal from 'sweetalert2';
+import { useLocation } from 'react-router';
 
 const UserPoint = () => {
-    const [selectPrice, setSelectPrice] = useState();
-    const [price, setPrice] = useState(0);
+    const accessToken = useSelector(state => state.persistedReducer.accessToken);
+    const member = useSelector(state => state.persistedReducer.member);
+    const payment = useSelector(state=>state.persistedReducer.payment.isSuccess);
+    const dispatch = useDispatch();
+    const [myPoint, setMyPoint] = useState(0); // 회원의 보유 포인트
+
+    const [selectPrice, setSelectPrice] = useState(0); // 선택한 가격 보여주기
+    const [price, setPrice] = useState(1000); // 페이먼트로 전달할 가격
+
+    // swal
+    const toast = Swal.mixin({
+        toast: true,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 1500
+    })
+
+    // 테스트
+    // const { updatePaymentInfo } = useContext(PaymentContext);
+    const [paymentModal, setPaymentModal] = useState(false);
+    const paymentData = {price: price, orderName: "포인트구매"};
+    const paymentOpen = () => {
+        if (paymentData.price === 0) {
+            toast.fire({
+              icon: 'info',
+              title: '충전할 금액을 선택해 주세요'
+            })
+            return;
+          }
+        setPaymentModal(true);
+    }
+    // 테스트 끝
+
     const coffeeData = [
         { beansCount: 10, price: 1000 },
         { beansCount: 30, price: 3000 },
@@ -19,17 +53,31 @@ const UserPoint = () => {
     const priceClick = (price, index) => {
         setPrice(price);
         setSelectPrice(index);
-        console.log(price); 
     };
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/point/${member.memNo}`,
+        {
+            headers : {
+                Authorization : accessToken
+            }
+        })
+        .then((res)=>{
+            setMyPoint(res.data);
+        })
+        .catch((error) =>{
+            console.log(error);
+        })
+    }, [])
 
     return (
         <div className='mypage'>
             <UserSideTab />
             <div className='userPointBox'>
                 <div className='nicknameBox'>
-                    <p><img src="/img/macaroon.png" alt="house" />바보수빈 님의 보유 커피콩</p>
+                    <p><img src="/img/macaroon.png" alt="house" />{member.nickname} 님의 보유 커피콩</p>
                     <p><img src="/img/coffeebeans.png" alt="house" /></p>
-                    <p>33개</p>
+                    <p>{myPoint}개</p>
                 </div>
 
                 <div className='coffeebeanBox'>
@@ -40,10 +88,15 @@ const UserPoint = () => {
                                 <p><img src="/img/coffeebeans.png" alt="coffebeans" />
                                 <img src="/img/X.png" alt="X" width={"25px"} height={"25px"} />{coffee.beansCount}</p>
                                 <div>{coffee.price}원</div>
+
                             </div>
                         ))}
                     </div>
-                    <div><CheckoutPage price={price}/></div>
+
+                    <button className='purchaseBtn' onClick={paymentOpen}>{price}원 결제</button>
+                    {paymentModal && (
+                        <CheckoutPage paymentData={paymentData} />)}
+                    {/* <div><CheckoutPage price={price}/></div> */}
                 </div>
 
 
