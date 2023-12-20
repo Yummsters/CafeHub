2641543842ef28cc90e13e6756e698c6a743e2bd
김희진 <95160590+Khjin06k@users.noreply.github.com>
2023-12-20 11:59:58 +0900

    ✏️modify : oauth2 에러 수정

--- server/src/main/java/com/yummsters/cafehub/global/auth/oauth2/OAuth2MemberInfo.java
+++ server/src/main/java/com/yummsters/cafehub/global/auth/oauth2/OAuth2MemberInfo.java
@@ -3,4 +3,6 @@ package com.yummsters.cafehub.global.auth.oauth2;
 public interface OAuth2MemberInfo {
     String getEmail();
     String getNickname();
+    String getProvider();
+    String getProviderId();
 }
