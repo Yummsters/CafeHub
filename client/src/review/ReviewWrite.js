d2b32072e3ec81415186592c4834776a8ba2d94b
soobin70 <142298498+soobin70@users.noreply.github.com>
2023-12-23 16:34:54 +0900

    리뷰등록 수정

--- client/src/review/ReviewWrite.js
+++ client/src/review/ReviewWrite.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Input } from 'reactstrap';
-import { useNavigate } from 'react-router-dom';
+import {useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
@@ -11,7 +11,10 @@ import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
 import Swal from 'sweetalert2';
 import { useSelector } from 'react-redux';
-import { getCookie } from '../components/Cookie';
+import { getCookie, setCookie, removeCookie } from '../components/Cookie';
+import {useDispatch} from 'react-redux';
+import { normalCheck, tokenCreate, tokenExpried } from "../login/TokenCheck.js";
+
 
 const ReviewWrite = () => {
     const [editorInstance, setEditorInstance] = useState(null);
@@ -26,45 +29,42 @@ const ReviewWrite = () => {
     const [thumbnail, setThumbnail] = useState(null);
     const [isFileSelected, setIsFileSelected] = useState(false);
     const token = useSelector(state => state.persistedReducer.accessToken);
-    const accessToken = useSelector(state => state.persistedReducer.accessToken);
     const memNo = useSelector(state => state.persistedReducer.member.memNo);
     const [selectedReviewAuthNo, setSelectedReviewAuthNo] = useState('');
     const [selectedCafeNo, setSelectedCafeNo] = useState('');
-
-
+    const dispatch = useDispatch();
+  //  const {accessToken, refreshToken} = useParams();
+  
+   
     useEffect(() => {
         if (token) {
-            console.log('현재 토큰:', token);
-            console.log(getCookie("refreshToken"));
-
-            // 토큰을 이용한 사용자 정보 가져오기
             axios.get(`http://localhost:8080/member`, {
                 headers: {
-                    Authorization: accessToken,
+                    Authorization: token,
                     Refresh: getCookie("refreshToken"),
                     'Content-Type': 'application/json'
                 }
             })
                 .then(response => {
-                    // 사용자 정보에서 원하는 값(review.writer)을 가져와 설정
                     const memNo = response?.data?.memNo;
                     if (memNo) {
                         setReview(prevReview => ({
                             ...prevReview,
                             writer: memNo,
                         }));
-                        console.log('사용자 정보:', response.data);
                         fetchCafeList();
                     } else {
-                        console.error('사용자 정보에서 memNo를 찾을 수 없습니다.');
+                        console.error('error');
                     }
                 })
                 .catch(error => {
-                    console.error('사용자 정보 가져오기 실패:', error);
-                    // 실패 시에 대한 처리를 추가할 수 있습니다.
+                    console.error(error);
                 });
         }
-    }, [token, memNo]);
+    }, [token, memNo, token, getCookie("refreshToken")]);
+    
+   
+    
 
     const uploadImages = (blob, callback) => {
         let formData = new FormData();
@@ -79,13 +79,10 @@ const ReviewWrite = () => {
             },
         })
             .then((response) => {
-                console.log('이미지 업로드 성공', response.data);
-
-                // 에디터에 이미지 추가
+              
                 callback(response.data);
             })
             .catch((error) => {
-                console.error('프론트 이미지 업로드 실패', error);
                 callback('image_load_failfff');
             });
     };
@@ -94,16 +91,23 @@ const ReviewWrite = () => {
 
     const fetchCafeList = async () => {
         try {
-            const response = await axios.get(`http://localhost:8080/reviewauth/${memNo}`);
-
+            const response = await axios.get(`http://localhost:8080/member/reviewauth/${memNo}`, {
+                headers: {
+                    Authorization: token,
+                    Refresh: getCookie("refreshToken")
+                }
+            });
             setCafes(response.data);
-            console.log('Cafes:', response.data);
         } catch (error) {
-            console.error('Error fetching cafe list:', error);
+            console.error(error);
         }
     };
+  
     const handleEditorChange = (content) => {
-        console.log(content);
+        setReview(prevReview => ({
+            ...prevReview,
+            content: content,
+        }));
     };
 
     const submit = (e) => {
@@ -118,39 +122,72 @@ const ReviewWrite = () => {
 
             return;
         }
+        if (selectedTags.length === 0) {
+            Swal.fire({
+                title: '태그를 선택하세요',
+                icon: 'error',
+                confirmButtonText: '확인',
+            });
+            return;
+        }
+        if (!review.title.trim()) {
+            Swal.fire({
+                title: '제목을 입력하세요',
+                icon: 'error',
+                confirmButtonText: '확인',
+            });
+            return;
+        }
+        if (!thumbnail) {
+            Swal.fire({
+                title: '썸네일을 등록하세요',
+                icon: 'error',
+                confirmButtonText: '확인',
+            });
+            return;
+        }
+    
+        const content = editorRef.current.getInstance().getHTML();
+       
+        if (!content.trim() || content.trim() === '<p><br></p>'){
+            Swal.fire({
+                title: '내용을 입력하세요',
+                icon: 'error',
+                confirmButtonText: '확인',
+            });
+            return;
+        }
 
         const formData = new FormData();
-        //title보내기
         formData.append('title', review.title);
-        const content = editorRef.current.getInstance().getHTML();
-        console.log('FormData의 콘텐츠:', content);
-        //content보내기
         formData.append('content', content);
-        //writer보내기
         formData.append('memNo', review.writer);
         formData.append('writer', review.writer);
-        //tag보내기
         formData.append('tagName', JSON.stringify(selectedTags));
-        console.log('태그보내짐?', JSON.stringify(selectedTags));
-        //cafeno보내기
-        formData.append('ReviewAuthNo', selectedReviewAuthNo); // 리뷰 인증 번호
-        console.log("reviewauthno보내지나", selectedReviewAuthNo);
+        formData.append('ReviewAuthNo', selectedReviewAuthNo);
         formData.append('cafeNo', selectedCafeNo);
-        console.log("카페번호", selectedCafeNo);
         if (selectedFile) {
             formData.append('file', selectedFile);
-            // formData.append('thumb_img', selectedFile);
         }
         if (thumbnail) {
             formData.append('thumbnail', thumbnail);
+            
         }
 
         axios
-            .post('http://localhost:8080/reviewwrite', formData)
-            .then((res) => {
+            .post(`http://localhost:8080/member/reviewwrite`, formData, {
+            headers : {
+                Authorization :token,
+                Refresh : getCookie("refreshToken")
+            }
+            
+           })
+           .then(res=>{
+            tokenCreate(dispatch, setCookie, res.headers)
+            .then(() => {
                 console.log(res);
                 let reviewNo = res.data;
-
+               
                 Swal.fire({
                     title: '커피콩 1개 적립 성공!',
                     text: '리뷰가 성공적으로 등록되었습니다',
@@ -160,29 +197,37 @@ const ReviewWrite = () => {
                     navigate(`/reviewList`);
                 });
             })
-            .catch((err) => {
-                console.log(err);
-
+        })
+        .catch(err=>{
+            if(err.response !== undefined){
+                tokenExpried(dispatch, removeCookie, err.response.data, navigate);
+            }else{
                 Swal.fire({
                     title: 'error',
                     text: '리뷰를 등록하는 중에 오류가 발생했습니다',
                     icon: 'error',
                     confirmButtonText: '확인',
-                });
-            });
-    };
+                })         
+            }
+               
+        })
+    }
 
     useEffect(() => {
-        axios.get(`http://localhost:8080/reviewTagList`)
-            .then(res => {
-                console.log(res.data);
-                setTagName([...res.data]);
-                console.log("태그이름" + tagName);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }, [])
+        axios.get('http://localhost:8080/reviewTagList', {
+          headers: {
+            Authorization: token,
+            Refresh: getCookie("refreshToken")
+          }
+        })
+        .then(res => {
+          setTagName([...res.data]);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      }, []);
+      
     const [selectedTags, setSelectedTags] = useState([]);
 
     const tagClick = (i) => {
@@ -227,7 +272,7 @@ const ReviewWrite = () => {
             setSelectedFile(file);
             const thumbnailURL = URL.createObjectURL(file);
             setThumbnail(thumbnailURL);
-            // setIsFileSelected(true);
+           
         }
     };
     const resetForm = () => {
@@ -240,7 +285,7 @@ const ReviewWrite = () => {
         setSelectedTags([]);
         setSelectedReviewAuthNo('');
         setSelectedCafeNo('');
-        //content초기화
+        
 
         editorRef.current.getInstance().setMarkdown('');
     };
@@ -280,8 +325,7 @@ const ReviewWrite = () => {
                 </div>
                 <hr className='line' />
                 <div className='thumbnail'>
-                    {/* <p className='review-thum'>썸네일 선택 &nbsp;&nbsp;&nbsp;</p> */}
-                    {!isFileSelected && (
+                      {!isFileSelected && (
                         <label className='review-img' >
                             썸네일 선택
                             <input type='file' name='thumbImg' onChange={handleFileChange} />
@@ -313,11 +357,9 @@ const ReviewWrite = () => {
                                     },
                                 })
                                     .then((response) => {
-                                        console.log('이미지 업로드 성공', response.data);
                                         callback(response.data);
                                     })
                                     .catch((error) => {
-                                        console.error('프론트 이미지 업로드 실패', error);
                                         callback('image_load_fail');
                                     });
                             },
@@ -333,7 +375,7 @@ const ReviewWrite = () => {
                             key={i}
                             className={selectedTags.includes(i) ? 'selectTag' : 'tag'}
                             onClick={() => tagClick(i)}>
-                            {review.tagNames}
+                            {tag.tagName}
                         </div>
                     ))}
                 </div>
