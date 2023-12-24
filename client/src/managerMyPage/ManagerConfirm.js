92a01e07629537c4bf50b752bbf3a8b17ac916d9
hyeri-seo <hye_dam_2@naver.com>
2023-12-24 11:46:04 +0900

    카페 목록 존재하지 않을지 처리

--- client/src/managerMyPage/ManagerConfirm.js
+++ client/src/managerMyPage/ManagerConfirm.js
@@ -72,40 +72,49 @@ const ManagerConfirm = () => {
             <ManagerSideTab />
             <div className='manager-listBox'>
                 <br /><label className='listTitle'>등록 가게 결제 확인</label><br /><br />
-                <Table hover>
-                    <tbody>
-                        {unpaidCafes.map((cafe) => (
-                            <tr key={cafe.cafeNo}>
-                                <th scope="row">
-                                    <img className='listImg' src={`/img/${cafe.thumbImg}.png`} alt='' />
-                                </th>
-                                <td colSpan={11}>
-                                    <div className='listMiniTitle'>{cafe.cafeName}</div>
-                                    <div className='description1'>{cafe.address}</div>
-                                </td>
-                                <td colSpan={1} className='permission-button'>
-                                    <button className='unpermission'>미결제</button>
-                                    <div className='manager-dateTime'>{cafe.paidDate}</div>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-                <div className='manager-pagination'>
-                    <ul className="pagination">
-                        <li className={`page-item ${pageInfo.currentPage === 1 ? 'disabled' : ''}`}>
-                            <button className="page-link" onClick={() => handlePageChange(pageInfo.currentPage - 1)}>&lt;</button>
-                        </li>
-                        {Array.from({ length: Math.ceil(pageInfo.endPage - pageInfo.startPage + 1) }, (_, index) => (
-                            <li key={index} className={`page-item ${pageInfo.currentPage === index + 1 ? 'active' : ''}`}>
-                                <button className="page-link" onClick={() => handlePageChange(index + pageInfo.startPage)}>{index + pageInfo.startPage}</button>
+                {unpaidCafes.length === 0 ? (
+                    <div>
+                        <p style={{marginLeft: "12%"}}>카페 목록이 존재하지 않습니다.</p>
+                    </div>
+                ) : (
+                    <Table hover>
+                        <tbody>
+                            {unpaidCafes.map((cafe) => (
+                                <tr key={cafe.cafeNo}>
+                                    <th scope="row">
+                                        <img className='listImg' src={`/img/${cafe.thumbImg}.png`} alt='' />
+                                    </th>
+                                    <td colSpan={11}>
+                                        <div className='listMiniTitle'>{cafe.cafeName}</div>
+                                        <div className='description1'>{cafe.address}</div>
+                                    </td>
+                                    <td colSpan={1} className='permission-button'>
+                                        <button className='unpermission'>미결제</button>
+                                        <div className='manager-dateTime'>{cafe.paidDate}</div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                )}
+
+                {unpaidCafes.length > 0 && (
+                    <div className='manager-pagination'>
+                        <ul className="pagination">
+                            <li className={`page-item ${pageInfo.currentPage === 1 ? 'disabled' : ''}`}>
+                                <button className="page-link" onClick={() => handlePageChange(pageInfo.currentPage - 1)}>&lt;</button>
                             </li>
-                        ))}
-                        <li className={`page-item ${pageInfo.currentPage === pageInfo.endPage ? 'disabled' : ''}`}>
-                            <button className="page-link" onClick={() => handlePageChange(pageInfo.currentPage + 1)}>&gt;</button>
-                        </li>
-                    </ul>
-                </div>
+                            {Array.from({ length: Math.ceil(pageInfo.endPage - pageInfo.startPage + 1) }, (_, index) => (
+                                <li key={index} className={`page-item ${pageInfo.currentPage === index + 1 ? 'active' : ''}`}>
+                                    <button className="page-link" onClick={() => handlePageChange(index + pageInfo.startPage)}>{index + pageInfo.startPage}</button>
+                                </li>
+                            ))}
+                            <li className={`page-item ${pageInfo.currentPage === pageInfo.endPage ? 'disabled' : ''}`}>
+                                <button className="page-link" onClick={() => handlePageChange(pageInfo.currentPage + 1)}>&gt;</button>
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </div>
         </div>
     );
