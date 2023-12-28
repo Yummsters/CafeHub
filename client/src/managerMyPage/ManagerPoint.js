0c6a2efd11e4392624e840fd7bd1ce5256b4c0fe
김희진 <95160590+Khjin06k@users.noreply.github.com>
2023-12-29 02:46:32 +0900

    css 및 오류 수정

--- client/src/managerMyPage/ManagerPoint.js
+++ client/src/managerMyPage/ManagerPoint.js
@@ -95,67 +95,74 @@ const Manager2 = () => {
         <div className='manager-container'>
             <ManagerSideTab />
             <div className='manager-listBox'>
-                <br /><label className='managerPointTitle'>포인트 정산</label><br /><br />
-                <Table hover>
-                    <tbody>
-                        {pointList.length != 0 && pointList.map(list => {
-                            return (
-                                <tr key={list.memNo}> 
-                                    <th scope="row"><img className='managerPoint-thumb' src={`${url}/common/upload/${list.thumbImg}`} alt=''/></th>
-                                    <td colSpan={2}> <div className='listMiniTitle' >{list.cafeName}</div></td>
-                                    <td colSpan={2}><div className='dateTime'>신청일 {list.refDate}</div></td>
-                                    <td colSpan={1}> <img className='managerPoint-bean' src='/img/coffeebeans.png' alt='' style={{ width: "40px" }} /></td>
-                                    <td colSpan={2}> {list.refPointCount}개 </td>
-                                    <td colSpan={2}> <button className='managerPoint-permission' id={list.memNo} onClick={permitPoint}>정산</button> </td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </Table>
-
-                <Pagination className="managerPoint-Page">
-                    <PaginationLink
-                        className='managerPoint-Button'
-                        onClick={() => { getPage(page - 1); setCurPage(page - 2); }}
-                        disabled={page === 1}>
-                        &lt;
-                    </PaginationLink>
-                    <PaginationLink
-                        className={`storeReview-Button ${firstNum === page ? 'current-page' : ''}`}
-                        onClick={() => getPage(firstNum)}
-                        aria-current={page === firstNum ? "page" : null}>
-                        {firstNum}
-                    </PaginationLink>
-                    {Array(total).fill().map((_, i) => {
-                        if (i <= 2) {
-                            return (
-                                <PaginationLink
-                                    className={`storeReview-Button ${firstNum + 1 + i === page ? 'current-page' : ''}`}
-                                    key={i + 1}
-                                    onClick={() => { getPage(firstNum + 1 + i) }}
-                                    aria-current={page === firstNum + 1 + i ? "page" : null}>
-                                    {firstNum + 1 + i}
-                                </PaginationLink>
-                            )
-                        } else if (i >= 3) {
-                            return (
-                                <PaginationLink
-                                    className={`storeReview-Button ${lastNum === page ? 'current-page' : ''}`}
-                                    key={i + 1}
-                                    onClick={() => getPage(lastNum)}
-                                    aria-current={page === lastNum ? "page" : null}>
-                                    {lastNum}
-                                </PaginationLink>
-                            )
-                        }
-                    })}
-                    <PaginationLink
-                        className='managerPoint-Button'
-                        onClick={() => { getPage(page + 1); setCurPage(page); }}
-                        disabled={page >= pageInfo.totalPages}>
-                        &gt;
-                    </PaginationLink>
-                </Pagination>
+                <br /><label className='managerPointTitle'>포인트 정산</label>
+                {pointList.length === 0 ? (
+                    <div>
+                        <p style={{textAlign : "center", fontSize : "20px", margin : "50px" }}>신청 목록이 존재하지 않습니다</p>
+                    </div>
+                ) : (
+                    <>
+                        <Table hover>
+                            <tbody>
+                                {pointList.length != 0 && pointList.map(list => {
+                                    return (
+                                        <tr key={list.memNo}> 
+                                            <th scope="row"><img className='managerPoint-thumb' src={`${url}/common/upload/${list.thumbImg}`} alt=''/></th>
+                                            <td colSpan={2}> <div className='listMiniTitle' >{list.cafeName}</div></td>
+                                            <td colSpan={2}><div className='dateTime'>신청일 {list.refDate}</div></td>
+                                            <td colSpan={1}> <img className='managerPoint-bean' src='/img/coffeebeans.png' alt='' style={{ width: "40px" }} /></td>
+                                            <td colSpan={2}> {list.refPointCount}개 </td>
+                                            <td colSpan={2}> <button className='managerPoint-permission' id={list.memNo} onClick={permitPoint}>정산</button> </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </Table>
+                        <Pagination className="managerPoint-Page">
+                            <PaginationLink
+                                className='managerPoint-Button'
+                                onClick={() => { getPage(page - 1); setCurPage(page - 2); }}
+                                disabled={page === 1}>
+                                &lt;
+                            </PaginationLink>
+                            <PaginationLink
+                                className={`storeReview-Button ${firstNum === page ? 'current-page' : ''}`}
+                                onClick={() => getPage(firstNum)}
+                                aria-current={page === firstNum ? "page" : null}>
+                                {firstNum}
+                            </PaginationLink>
+                            {Array(total).fill().map((_, i) => {
+                                if (i <= 2) {
+                                    return (
+                                        <PaginationLink
+                                            className={`storeReview-Button ${firstNum + 1 + i === page ? 'current-page' : ''}`}
+                                            key={i + 1}
+                                            onClick={() => { getPage(firstNum + 1 + i) }}
+                                            aria-current={page === firstNum + 1 + i ? "page" : null}>
+                                            {firstNum + 1 + i}
+                                        </PaginationLink>
+                                    )
+                                } else if (i >= 3) {
+                                    return (
+                                        <PaginationLink
+                                            className={`storeReview-Button ${lastNum === page ? 'current-page' : ''}`}
+                                            key={i + 1}
+                                            onClick={() => getPage(lastNum)}
+                                            aria-current={page === lastNum ? "page" : null}>
+                                            {lastNum}
+                                        </PaginationLink>
+                                    )
+                                }
+                            })}
+                            <PaginationLink
+                                className='managerPoint-Button'
+                                onClick={() => { getPage(page + 1); setCurPage(page); }}
+                                disabled={page >= pageInfo.totalPages}>
+                                &gt;
+                            </PaginationLink>
+                        </Pagination>
+                        </>
+                        )}
             </div>
         </div>
     );
